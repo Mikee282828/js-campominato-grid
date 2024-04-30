5a6357a7aa6a2f9a0641b957a404e4f453191ab9
michele laptop <micheledong2803@gmail.com>
2024-04-30 17:06:14 +0200

    m4 - createSquare & clickPulsante

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 let griglia = document.getElementById("griglia");
 
 for(let i = 1; i <= 100; i++){
+
+    // creo elemento e lo metto all'interno della griglia
+    let elemento = createSquare(i);
+    griglia.append(elemento);
+
+    //al click fa cose...
+    clickPulsante(elemento,i)
+
+}
+
+function createSquare(nIterazione){
     let element = document.createElement("div");
     element.classList.add("square");
-    element.append(i);
-    griglia.append(element);
+    element.append(nIterazione);
+    return element;
+}
+
+function clickPulsante(element,nIterazione){
     element.addEventListener("click",function(){
         element.classList.toggle("bkg_azzurro");
-        console.log(i);
+        console.log(nIterazione);
     })
 }
